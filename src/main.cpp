dab799f6a90bf2ec5561baa16884c5ed9cd4e9f6
a.kain <a.kain@vktech.ru>
2026-09-13 17:52:57 +0300

    add joystick read pos and button

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,24 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const int axisX = A1;  // ось X (VRx) подключена к A1
+const int axisY = A0;  // ось Y (VRy) подключена к A0
+const int buttonPin = 7;  // кнопка джойстика подключена к D7
+int valX, valY, valButton = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    pinMode(buttonPin, INPUT_PULLUP);  // D7 как вход с подтягивающим резистором
+    Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    valX = analogRead(axisX);   // значение оси X
+    valY = analogRead(axisY);   // значение оси Y
+    valButton = 1 - digitalRead(buttonPin);  // кнопка (инвертированное значение)
+    Serial.print("X:");
+    Serial.print(valX, DEC);
+    Serial.print(" | Y:");
+    Serial.print(valY, DEC);
+    Serial.print(" | Button: ");
+    Serial.println(valButton, DEC);
+    delay(30);
 }
